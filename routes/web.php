63f6ddaf1b8b3469de19a5190b055a5401b87c9c
Yotman Reyes <yotman.reyes@gmail.com>
2022-07-02 06:44:05 -0400

    add new routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
 /** @var \Laravel\Lumen\Routing\Router $router */
 
 /*
@@ -15,4 +16,21 @@ use Illuminate\Support\Facades\DB;
 
 $router->get('/', function () use ($router) {
     return 'api';
+});
+
+$router->post('/agregar-vendedor', function (Request $request) use ($router){
+    DB::table('/agregar-vendedor')->insert([
+        'nombre' => $request->post('nombre'),
+        'apellidos' => $request->post('apellidos'),
+        'cedula'    => $request->post('cedula')
+    ]);
+});
+
+$router->post('/vendedor/{id}', function ($id) use ($router){
+    $vendedor = DB::table('vendedor')->where('id', $id)->first();
+
+    if($vendedor == null){
+        return http_response_code(403);
+    }
+    return json_encode($vendedor);
 });
